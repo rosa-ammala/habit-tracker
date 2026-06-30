41539a53c90fa7cae5784a29432e6199fc3f98ae
rosa-ammala <rosa.ammala@gmail.com>
2026-06-30 10:50:39 +0300

    Vite React TS and Tailwind installation and setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-2xl font-semibold text-gray-900">
+        Habit Tracker v2
+      </h1>
+    </main>
+  );
+}
+
+export default App;
